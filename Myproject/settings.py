c7d86a2481391289a475b6b80320b0ee5bfb15fe
beatandbase <beatandbase01@gmail.com>
2024-10-20 23:57:30 +0530

    Update settings.py

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -12,9 +12,9 @@ from decouple import config
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 DOMAIN_NAME = config('DOMAIN_NAME')
 
